94ca74d4a981951d0bb5b3f30441538b0ea8e1c5
Harry-Crocksick <harrycrocksick1inamillion@gmail.com>
2023-12-21 14:33:28 +0630

    New: added role based authentication & authorization

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,36 +1,64 @@
-import type { NextAuthOptions } from 'next-auth';
+import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
+import { GithubProfile } from "next-auth/providers/github";
 
 export const options: NextAuthOptions = {
-	providers: [
-		GitHubProvider({
-		    clientId: process.env.GITHUB_ID as string,
-		    clientSecret: process.env.GITHUB_SECRET as string
-	  	}),
-	  	CredentialsProvider({
-	  		name: 'Credentials',
-	  		credentials: {
-	  			username: {
-	  				label: 'Username:', 
-	  				type: 'text',
-	  				placeholder: 'Your username'
-	  			},
-	  			password: {
-	  				label: 'Password:',
-	  				type: 'password',
-	  				placeholder: 'Password'
-	  			}
-	  		},
-	  		async authorize(credentials, req) {
-	  			const user = { id: '42', name: 'Dave', password: 'nextauth' };
+  providers: [
+    GitHubProvider({
+      profile(profile: GithubProfile) {
+        console.log(profile);
+        return {
+          ...profile,
+          role: profile.role ?? "user",
+          id: profile.id.toString(),
+          image: profile.avatar_url,
+        };
+      },
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "Your username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: "42",
+          name: "Dave",
+          password: "nextauth",
+          role: "admin",
+        };
 
-	  			if(credentials?.username === user.name && credentials?.password === user.password) {
-	  				return user;
-	  			} else {
-	  				return null;
-	  			}
-	  		}
-	  	})
-	],
-}
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,31 @@
-export { default } from 'next-auth/middleware';
+import { NextResponse } from "next/server";
+import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
 
-export const config = { matcher: ['/extra', '/dashboard'] };
+export default withAuth(
+  function middleware(request: NextRequestWithAuth) {
+    console.log(request.nextUrl.pathname);
+    console.log(request.nextauth.token);
+
+    if (
+      request.nextUrl.pathname.startsWith("/extra") &&
+      request.nextauth.token?.role !== "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+
+    if (
+      request.nextUrl.pathname.startsWith("/client") &&
+      request.nextauth.token?.role !== "admin" &&
+      request.nextauth.token?.role !== "manager"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
+
+export const config = { matcher: ["/extra", "/dashboard", "/client"] };
