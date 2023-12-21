59ce1e905cdce0213375b19649f0c5e1abb6fa6e
Harry-Crocksick <harrycrocksick1inamillion@gmail.com>
2023-12-21 12:31:37 +0630

    Implementing: Authentication with NextAuth.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
-import Image from 'next/image'
+'use client';
+import {Fragment} from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import { options } from './api/auth/[...nextauth]/options'
+// import { getServerSession } from 'next-auth';
+import {redirect} from 'next/navigation';
 
 export default function Home() {
+  // const session = await getServerSession(options);
+  const {data: session} = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect('/api/auth/signin?callbackUrl=/server')
+    }
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Fragment>
+    {
+      session ? (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+          Get started by <Link href="/api/auth/signout" className="text-md underline font-semibold text-cyan-500">Sign Out</Link>{" "}
           <code className="font-mono font-bold">app/page.tsx</code>
+          <Image src={session?.user.image} width="75" height="75" alt="Avatar" priority />
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -109,5 +128,8 @@ export default function Home() {
         </a>
       </div>
     </main>
+      ) : (<h1>You shall not pass without signing in</h1>)
+    }
+    </Fragment>
   )
 }
